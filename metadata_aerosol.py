0f10890e9f96bef1c88554a08557e9e2d07dde35
Jack Yu <llacque@gmail.com>
2016-01-12 09:29:18 +0800

    aerosol sw effects. tables and links working.

---
+++ metadata_aerosol.py
@@ -0,0 +1,49 @@
+
+
+
+def sw_concurrent():
+    rtms = [{'reference': 'RTM', 'model_name': 'RRTMG',
+             'aerosol_in_sw': 'yes', 'aerosol_in_lw': 'no'},
+            {'reference': 'RTM1', 'model_name': 'RRTMG',
+             'aerosol_in_sw': 'no', 'aerosol_in_lw': 'no'},
+            {'reference': 'RTM2', 'model_name': 'CLIRAD',
+             'aerosol_in_sw': 'yes', 'aerosol_in_lw': 'no'},
+            {'reference': 'RTM3', 'model_name': 'CLIRAD',
+             'aerosol_in_sw': 'no', 'aerosol_in_lw': 'no'}]
+    hrefs = [{'href': 'http://nbviewer.ipython.org/github/qAp/SCAM_radiation_notebooks/blob/master/arm95_LW_RRTMG_RRTMG_SW_RRTMGaer_RRTMG.ipynb',
+              'label': 'RTM - RTM1'},
+             {'href': 'http://nbviewer.ipython.org/github/qAp/SCAM_radiation_notebooks/blob/master/arm95_-_LW_RRTMG_SW_RRTMGaer_-_LW_CLIRAD_CLIRAD_SW_CLIRADaer_CLIRAD.ipynb',
+              'label': 'RTM2 - RTM3'}]
+    return {'rtms': rtms, 'hrefs': hrefs}
+
+
+def sw_clirad_aeryes():
+    rtms = [{'reference': 'RTMyes', 'model_name': 'CLIRAD',
+             'aerosol_in_sw': 'yes', 'aerosol_in_lw': 'no'}]
+    hrefs = [{'href': 'http://nbviewer.ipython.org/github/qAp/SCAM_radiation_notebooks/blob/master/arm95_LW_CLIRAD_SW_CLIRAD_vs_LW_CLIRAD_SW_CLIRADaer.ipynb',
+              'label': 'RTMyes - RTMno'}]
+    return {'rtms': rtms, 'hrefs': hrefs}
+
+
+def sw_clirad_aerno():
+    rtms = [{'reference': 'RTMno', 'model_name': 'CLIRAD',
+             'aerosol_in_sw': 'no', 'aerosol_in_lw': 'no'}]
+    hrefs = [{'href': 'http://nbviewer.ipython.org/github/qAp/SCAM_radiation_notebooks/blob/master/arm95_LW_CLIRAD_SW_CLIRAD_vs_LW_CLIRAD_SW_CLIRADaer.ipynb',
+              'label': 'RTMyes - RTMno'}]
+    return {'rtms': rtms, 'hrefs': hrefs}
+
+
+def sw_rrtmg_aeryes():
+    rtms = [{'reference': 'RTMyes', 'model_name': 'RRTMG',
+             'aerosol_in_sw': 'yes', 'aerosol_in_lw': 'no'}]
+    hrefs = [{'href': 'http://nbviewer.ipython.org/github/qAp/SCAM_radiation_notebooks/blob/master/arm95_LW_RRTMG_SW_RRTMG_vs_LW_RRTMG_SW_RRTMGaer.ipynb',
+              'label': 'RTMyes - RTMno'}]
+    return {'rtms': rtms, 'hrefs': hrefs}
+
+
+def sw_rrtmg_aerno():
+    rtms = [{'reference': 'RTMno', 'model_name': 'RRTMG',
+              'aerosol_in_sw': 'no', 'aerosol_in_lw': 'no'}]
+    hrefs = [{'href': 'http://nbviewer.ipython.org/github/qAp/SCAM_radiation_notebooks/blob/master/arm95_LW_RRTMG_SW_RRTMG_vs_LW_RRTMG_SW_RRTMGaer.ipynb',
+              'label': 'RTMyes - RTMno'}]
+    return {'rtms': rtms, 'hrefs': hrefs}
---
+++ projectpage.py
@@ -0,0 +1,99 @@
+
+import os
+
+import jinja2
+
+import metadata_aerosol
+
+
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+package_loader = jinja2.PackageLoader('projectpage', 'templates')
+jinja_env = jinja2.Environment(loader = package_loader)
+
+
+def page_index():
+    template_index = jinja_env.get_template('index.html')
+    
+    with open('index.html', mode = 'w', encoding = 'utf-8') as file:
+        file.write(template_index.render())
+
+
+def tablerows_hrefs_2_panel(tables=None, hrefs=None):
+    if not (tables and hrefs):
+        return
+
+    template_table =  jinja_env.get_template('table_data_model_SW_LW.html')
+    template_hrefbutton = jinja_env.get_template('button_href.html')
+    
+    d = {}
+    d['body'] = ' '.join(template_table.render(rows=table)
+                         for table in tables)
+    d['footer'] = ' '.join(\
+        template_hrefbutton.render(href=href['href'], label=href['label'])
+                                         for href in hrefs)
+    return d
+
+    
+def html_aerosol_sw_effects():
+    '''
+    Returns HTML of Bootstrap panels summarising runs related
+    to the effect of including aerosol in shortwave radiation models.
+    '''
+    template_panel = jinja_env.get_template('panel_body_footer.html')
+
+    # panel: concurrent RTMs
+    d_concurrent = metadata_aerosol.sw_concurrent()
+    d_concurrent = tablerows_hrefs_2_panel(tables=[d_concurrent['rtms']],
+                                               hrefs=d_concurrent['hrefs'])
+
+    # panel: CLIRADs
+    d_clirad_aeryes = metadata_aerosol.sw_clirad_aeryes()
+    d_clirad_aerno = metadata_aerosol.sw_clirad_aerno()
+    d_clirad = tablerows_hrefs_2_panel(tables=[d_clirad_aeryes['rtms'], \
+                                               d_clirad_aerno['rtms']],
+                                       hrefs=d_clirad_aeryes['hrefs'])
+
+    # panel: RRTMGs
+    d_rrtmg_aeryes = metadata_aerosol.sw_rrtmg_aeryes()
+    d_rrtmg_aerno = metadata_aerosol.sw_rrtmg_aerno()
+    d_rrtmg = tablerows_hrefs_2_panel(tables=[d_rrtmg_aeryes['rtms'], \
+                                              d_rrtmg_aerno['rtms']],
+                                      hrefs=d_rrtmg_aeryes['hrefs'])
+
+    # render the HTML for each panel
+    panels = [d_concurrent, d_clirad, d_rrtmg]
+    html_panels = (template_panel.render(panel=panel) for panel in panels)
+    
+    return html_panels
+
+
+def page_aerosol():
+    '''
+    Generates string for aerosol.html
+    '''
+    template_aerosol = jinja_env.get_template('aerosol.html')
+    
+    html_panels = html_aerosol_sw_effects()
+    html_aerosol = template_aerosol.render(panels = html_panels)
+    
+    with open('aerosol.html', mode = 'w', encoding = 'utf-8') as file:
+        file.write(html_aerosol)
+
+        
+
+def keeps():
+    template = jinja_env.get_template('table_data_modeal_SW_LW.html')
+    print(template.render(rows = [{'reference': 'RTM',
+                                   'model_name': 'CLIRAD',
+                                   'aerosol_in_sw': 'Yes',
+                                   'aerosol_in_lw': 'No'}]))    
+
+
+
+
+
+if __name__ == '__main__':
+    page_index()
+    page_aerosol()
+
